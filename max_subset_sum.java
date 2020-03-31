66afa57ecfa08913bf10132efbbb2ae02e80e8d0
Govind Saxena <govindsaksenaji@gmail.com>
2020-03-31 12:18:48 +0530

    Add files via upload
    Maximum subset sum problem is a direct implementation of KADANE'S Algorithm

---
+++ max_subset_sum.java
@@ -0,0 +1,30 @@
+// KADANE'S ALOGORITHM
+import java.util.*;
+public class max_subset_sum {
+    public static void main(String args[]){
+    int a[]={-2,-3,4,-1,-2,1,5,-3};
+    
+    System.out.println("Maximum sum is: "+ kadane(a));
+    }
+    
+    static int kadane(int x[]){
+    int local_max=x[0],global_max=x[0];
+    
+    for(int i=1;i<x.length-1;i++){
+      local_max=Math.max(x[i],x[i]+local_max);
+       if(local_max>=global_max){
+       global_max=local_max;
+       }    
+    
+    
+    }
+    
+    
+    
+    return global_max;
+        
+        
+        
+    
+    }
+ }
