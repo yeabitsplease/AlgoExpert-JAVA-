de73606dcc6273b535a7865b8f7954ae46acf26a
Govind Saxena <govind.18bci1043@abes.ac.in>
2020-12-05 16:30:52 +0530

    Add files via upload

---
+++ ThreeLargestNumbers.java
@@ -0,0 +1,41 @@
+/*
+
+Write a function that takes in array of integers and return the three largest numbers in the array without sorting them in position.
+
+*/
+
+
+import java.util.*;
+
+class ThreeLargestNumbers{
+  public static int[] findThreeLargestNumbers(int[] array) {
+    int[] threeLargest={Integer.MIN_VALUE,Integer.MIN_VALUE,Integer.MIN_VALUE};
+		for(int num: array){
+			updateLargest(threeLargest,num);
+		}
+    return threeLargest;
+  }
+	
+	public static void updateLargest(int[] threeLargest, int num){
+		if(num>threeLargest[2]){
+			shiftAndUpdate(threeLargest,num,2);
+		}
+		else if(num>threeLargest[1]){
+			shiftAndUpdate(threeLargest,num,1);
+		}
+		else if(num>threeLargest[0]){
+			shiftAndUpdate(threeLargest,num,0);
+		}
+		}
+	
+	public static void shiftAndUpdate(int[] array, int num, int idx){
+		for(int i=0;i<=idx;i++){
+			if(i==idx){
+				array[i]=num;
+			}
+			else{
+				array[i]=array[i+1];
+			}
+		}
+	}
+}
