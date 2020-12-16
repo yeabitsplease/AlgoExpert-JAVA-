f3cb3f6a18a954562afe8ecdf0043632c4156a2d
Govind Saxena <govind.18bci1043@abes.ac.in>
2020-12-16 17:27:24 +0530

    Add files via upload

---
+++ FourNumberSum.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class FourNumberSum {
+  public static List<Integer[]> fourNumberSum(int[] array, int targetSum) { 
+   Map<Integer,List<Integer[]>> allPairSums=new HashMap<>();
+		List<Integer[]> quadruplets=new ArrayList<Integer[]>();
+		for(int i=1;i<array.length-1;i++){
+			for(int j=i+1;j<array.length;j++){
+				int currentSum=array[i]+array[j];
+				int difference=targetSum-currentSum;
+				if(allPairSums.containsKey(difference)){
+					for(Integer[] pair: allPairSums.get(difference)){
+						Integer[] newQaudruplet={pair[0],pair[1],array[i],array[j]};
+						quadruplets.add(newQaudruplet);
+					}
+				}
+			}
+			
+			for(int k=0;k<i;k++){
+				int currentSum=array[i]+array[k];
+				Integer[] pair={array[k],array[i]};
+				if(!allPairSums.containsKey(currentSum)){
+					List<Integer[]> pairGroup=new ArrayList<Integer[]>();
+					pairGroup.add(pair);
+					allPairSums.put(currentSum,pairGroup);
+				}
+				else{
+					allPairSums.get(currentSum).add(pair);
+				}
+			}
+		}
+		return quadruplets;
+  }
+}
