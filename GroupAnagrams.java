6ca590ab732f06bb524ecf388f442268b3e212c6
Govind Saxena <govind.18bci1043@abes.ac.in>
2020-12-21 22:28:18 +0530

    Update GroupAnagrams.java

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -39,9 +39,8 @@ class GroupAnagrams{
 	
 	
 	public static void main(String args[]){
-		/*
-		
-		------TEST CASE1------
+			
+		/* ------TEST CASE1------ */
 		
 		List<String> words =
         new ArrayList<String>(Arrays.asList("yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"));
@@ -49,12 +48,46 @@ class GroupAnagrams{
 		List<List<String>> expected = groupAnagrams(words);
 		for(int i=0;i<expected.size();i++){
           System.out.println(expected.get(i));
-		} 
-		*/
-		
-		
-		
-		
+		} 	
 		
 	}
 }
+
+
+/*
+------ Test Stub ------
+
+import java.util.*;
+
+class ProgramTest {
+  @Test
+  public void TestCase1() {
+    List<String> words =
+        new ArrayList<String>(
+            Arrays.asList("yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"));
+    List<List<String>> expected = new ArrayList<List<String>>();
+    expected.add(new ArrayList<String>(Arrays.asList("yo", "oy")));
+    expected.add(new ArrayList<String>(Arrays.asList("flop", "olfp")));
+    expected.add(new ArrayList<String>(Arrays.asList("act", "tac", "cat")));
+    expected.add(new ArrayList<String>(Arrays.asList("foo")));
+    List<List<String>> output = GroupAnagrams.groupAnagrams(words);
+    for (List<String> innerList : output) {
+      Collections.sort(innerList);
+    }
+    Utils.assertTrue(compare(expected, output));
+  }
+
+  public boolean compare(List<List<String>> expected, List<List<String>> output) {
+    if (expected.size() != output.size()) return false;
+
+    for (List<String> group : expected) {
+      Collections.sort(group);
+      if (!output.contains(group)) return false;
+    }
+
+    return true;
+  }
+}
+
+*/
+
