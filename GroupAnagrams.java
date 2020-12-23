ff8a9cbc065cf7244026cb50f5b90c9d7fda22df
Govind Saxena <govind.18bci1043@abes.ac.in>
2020-12-23 23:48:42 +0530

    Update GroupAnagrams.java

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -36,6 +36,24 @@ class GroupAnagrams{
 		return result;
 	}
 	
+	/* SECOND WAY TO DO THIS QUESTION */
+	
+	 public static List<List<String>> groupAnagrams(List<String> words) {
+    Map<String, List<String>> anagrams=new HashMap<String, List<String>>();
+		
+		for(String word: words){
+			char[] charArray=word.toCharArray();
+			Arrays.sort(charArray);
+			String sortedWord=new String(charArray);
+			
+			if(anagrams.containsKey(sortedWord)){
+				anagrams.get(sortedWord).add(word);
+			} else{
+				anagrams.put(sortedWord, new ArrayList<String>(Arrays.asList(word)));
+			}
+		}
+		return new ArrayList<>(anagrams.values());
+  }
 	
 	
 	public static void main(String args[]){
