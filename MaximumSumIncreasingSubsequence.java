6b9bfccdd0923f91bfce09d966ed96c2f8649a3b
Govind Saxena <govind.18bci1043@abes.ac.in>
2021-03-21 12:12:22 +0530

    Add files via upload

---
+++ MaximumSumIncreasingSubsequence.java
@@ -0,0 +1,46 @@
+/*
+
+Write a function that takes in a non-empty array of integers and retuens the greatest sum that can be 
+generated from a strictly increasing subsequence in the array as well as an array of the numbers in that subsequence.
+
+*/
+
+
+import java.util.*;
+
+class MaximumSumIncreasingSubsequence {
+  public static List<List<Integer>> maxSumIncreasingSubsequence(int[] array) {
+  	int[] sequences=new int[array.length];
+		Arrays.fill(sequences,Integer.MIN_VALUE);
+		int sums[]=array.clone();
+		int maxSumIdx=0;
+		for(int i=0;i<array.length;i++){
+			int currentNum=array[i];
+			for(int j=0;j<i;j++){
+				int otherNum=array[j];
+				if(otherNum<currentNum && sums[j]+currentNum>=sums[i]){
+					sums[i]=sums[j]+currentNum;
+					sequences[i]=j;
+				}
+			}
+			if(sums[i]>=sums[maxSumIdx]){
+				maxSumIdx=i;
+			}
+		}
+		return buildSequence(array, sequences, maxSumIdx, sums[maxSumIdx]);
+	
+  }
+	
+	public static List<List<Integer>> buildSequence(int[] array, int[] sequences, int currentIdx, int sums){
+		List<List<Integer>> sequence=new ArrayList<List<Integer>>();
+		sequence.add(new ArrayList<Integer>());
+		sequence.add(new ArrayList<Integer>());
+		sequence.get(0).add(sums);
+		while(currentIdx!=Integer.MIN_VALUE){
+			sequence.get(1).add(0,array[currentIdx]);
+			currentIdx=sequences[currentIdx];
+		}
+		return sequence;
+	}
+	
+}
